4d4d1db6409c30f934e6e2a4165cfc47c7cfc717
sardor <sardor@insilicogen.com>
2016-05-09 13:51:45 +0900

    Updated: testcase

--- tests/test_devon.py
+++ tests/test_devon.py
@@ -10,20 +10,28 @@ Tests for `devon` module.
 
 import unittest
 
-from devon.devon import WordProcessor
+from devon.devon import WordProcessor, FSMStemmer
 
 
 class TestWordProcessor(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.corpora_lat = """
+        Barcha insonlar erkin, qadr-qimmat va huquqlarda tang bo‘lib tug‘iladilar. Ular aql va vijdon sohibidirlar va bir-birlariga birodarlarcha muomala qilishlari zarur.
+        """
+        self.corpora_cyr = """
+        Барча инсонлар эркин, қадр-қиммат ва ҳуқуқларда танг бўлиб туғиладилар. Улар ақл ва виждон соҳибидирлар ва бир-бирларига биродарларча муомала қилишлари зарур.
+        """
 
     def tearDown(self):
-        pass
+        del self.corpora_lat
+        del self.corpora_cyr
 
     def test_stem(self):
-            self.assertEqual(
-                    WordProcessor.stem(word='mahallalarimizdan'), ('mahalla'))
+        stemmer = FSMStemmer()
+        stems = stemmer.stem(words=self.corpora_lat, multi_words=True)
+        expectation = ['Barcha', 'inson', 'erkin,', 'qadr-qimmat', 'va', 'huquq', 'ta', 'bo‘lib', 'tug‘iladilar.', 'U', 'aql', 'va', 'vijdon', 'sohibidir', 'va', 'bir-bir', 'birodarlarcha', 'muomala', 'qilish', 'zarur.']
+        self.assertEqual(stems, expectation)
 
     def test_segmentize(self):
         self.assertEqual(
